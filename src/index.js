1edcd37e2c882234e4d195ec8978127ae0cdd479
Rahul Kumar <rahulkumar2048@gmail.com>
2019-01-09 14:53:23 +0530

    Add mongoose

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+const logger = require('./config/logger');
 const { port, env } = require('./config/vars');
 const app = require('./config/express');
-const logger = require('./config/logger');
+const db = require('./config/mongoose');
 
 // listen to requests
 app.listen(port, () => logger.info(`server started on port ${port} (${env})`));
